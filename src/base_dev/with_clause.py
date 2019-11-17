034f4103d76c121a14dd935dfc3928c502738e84
sean(xiaojun) <xiaojun1@xiaomi.com>
2019-11-17 19:44:12 +0800

    【update】 add with condition

--- src/base_dev/with_clause.py
+++ src/base_dev/with_clause.py
@@ -16,7 +16,7 @@ class func(object):
         print traceback.format_exc(trace)
         # return True       # 使用返回值True捕获with中抛出的不同的异常，异常不会抛出with上下文
         return 1            # 同上
-        # return 0          # 使用返回值False(0)抛出异常，异常会抛出with上下文
+        # return 0          # 使用返回值False(0)(None)抛出异常，异常会抛出with上下文
 
 
 if __name__ == "__main__":
